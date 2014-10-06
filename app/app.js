6b7831dd938734e40e985cd0b5822701356d9771
Khalid Harun <khalid.harun@gmail.com>
2014-10-06 17:36:00 -0400

    moved dummy data from frontend to backend

--- app/app.js
+++ app/app.js
@@ -1,31 +1,32 @@
 var app = angular.module('topTerms', []);
 
-var dummyData = {
-  statuses: [
-    'hello there',
-    'I think',
-    'We have something here'
-  ],
-  topTerms: [
-    {
-      name: 'hello',
-      count: 50
-    },
-    {
-      name: 'there',
-      count: 30
-    },
-    {
-      name: 'buddy',
-      count: 20
-    }
-  ]
-};
+app.controller('TwitterStream', ['$scope', '$http', function($scope, $http) {
 
-app.controller('TwitterStream', ['$scope', function($scope) {
-  $scope.statuses = dummyData.statuses;
+  // Initialize statuses locally
+  $scope.statuses = [];
+
+  // Initialize topTerms from server
+  $http.get('/api/statuses').then(function(result) {
+    // Success
+    $scope.statuses = result.data;
+  }, function(reason) {
+    // Error
+    console.error('ERROR', reason);
+  });
+
 }]);
 
-app.controller('TopTerms', ['$scope', function($scope) {
-  $scope.topTerms = dummyData.topTerms;
+app.controller('TopTerms', ['$scope', '$http', function($scope, $http) {
+  // Initialize topTerms locally
+  $scope.topTerms = [];
+
+  // Initialize topTerms from server
+  $http.get('/api/topTerms').then(function(result) {
+    // Success case
+    $scope.topTerms = result.data;
+  }, function(reason) {
+    // Error case
+    console.error("ERROR", reason);
+  });
+
 }]);
--- server.js
+++ server.js
@@ -2,8 +2,42 @@ var express = require('express');
 var app = express();
 var port = 9000;
 
+var dummyData = {
+  statuses: [
+    'hello there',
+    'I think',
+    'We have something here'
+  ],
+  topTerms: [
+    {
+      name: 'hello server',
+      count: 50
+    },
+    {
+      name: 'there',
+      count: 30
+    },
+    {
+      name: 'buddy',
+      count: 20
+    }
+  ]
+};
+
 app.use(express.static('app'));
 app.use(express.static('bower_components'));
+
+app.get('/api/topTerms', function(req, res) {
+
+  // Send dummyData
+  res.send(dummyData.topTerms);
+});
+
+app.get('/api/statuses', function(req, res) {
+  // Send dummyData
+  res.send(dummyData.statuses);
+});
+
 
 app.listen(port);
 console.log("Server started listening on http://localhost:"+ port);
