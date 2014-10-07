796774aa93440b1b174cb48d3601c04ff15abaa1
Khalid Harun <khalid.harun@gmail.com>
2014-10-07 13:17:10 -0400

    Add comments to topterms.js

--- server/topterms.js
+++ server/topterms.js
@@ -5,17 +5,27 @@ var _ = require('underscore'),
  events  = require('events'),
  util    = require("util");
 
-// additional stopwords
+/* 
+*  Handle stop words
+*  I found an npm package with a list of stop words in English and Spanish.
+*  And them I added some of my own.
+*/
 var customStopWords = ['http', 'https', 'amp', "i'm", "we're", 'lol', "it's", "don't"];
 var stopwords = _.union(StopWords.english, StopWords.spanish, customStopWords);
 
+
+/*
+*  TopTerms - Constructor
+*/
 var TopTerms = function() {
   this.allTerms = {};
   this.statusBuffer = [];
   this.topTerms = [];
 };
 
-// Top Terms Logic
+/*
+*  Top Terms Logic
+*/
 
 TopTerms.prototype.reset = function() {
   this.allTerms = {};
@@ -30,38 +40,45 @@ TopTerms.prototype.getTopTerms = function() {
   return topTen;
 }
 
+/*
+*  Sends latest statuses and then clears the buffer of pending status messages
+*/
 TopTerms.prototype.getLatestStatuses = function() {
   var latestStatuses = this.statusBuffer;
   this.statusBuffer = [];
   return latestStatuses;
 }
 
+
+/*
+*  Sort the counter of allTerms and return an array of the top 10
+*/
 TopTerms.prototype.updateTopTerms = function() {
-  var self = this;
-
-  // Sort counter
-  var counter = _.pairs(self.allTerms);
+  var counter = _.pairs(this.allTerms);
   sorted = _.sortBy(counter, function(item) {return item[1];});
-
-  // return topTen
   return sorted.slice(10);
 
 };
 
 var statusCounter = 0;
 var statusFilter = /[\w\u00C0-\u04ff']{3,}/g;
+
 TopTerms.prototype.processStatus = function(status) {
+ 
+  // Sanitize and tokenize status message
   status = status.trim();
   console.log(statusCounter++, status);
   var words = status.toLowerCase().match(statusFilter); //.split(/\W/);
-  if(!words) return;
+ 
+  if(!words) return;  // return if no suitable words are found.
 
   var allTerms = this.allTerms;
-  words.forEach( function(item) {
 
+  words.forEach( function(item) {
+   
     // skip stop words
     if(_.contains(stopwords, item)) return;
-
+   
     // increment counter
     allTerms[item] = (allTerms[item]) ? allTerms[item] + 1: 1;
   });
@@ -70,7 +87,18 @@ TopTerms.prototype.processStatus = function(status) {
   this.statusBuffer.push(status);
 }
 
-// Twitter Stream
+
+
+/*
+* Twitter Stream Functionality
+*
+* In order to grab Twitter stream, I forked another npm package 'user-stream', which provides a really nice 
+* Twitter stream client with already handled the stream processing through OAuth requests.  The limitation of
+* this package is that it was only good for User Streams and not Public Streams.  I made a more generic 
+* version of this package that is able to handle any given Twitter stream endpoint at https://github.com/khalidharun/twitter-stream
+*
+* I'm using the Filter Stream endpoint and limiting my stream based on location.  Some example coordinate blocks are below.
+*/
 
 var geoLocations = {
     chicago: '-88,41,-87,42', // Chicago
@@ -79,6 +107,12 @@ var geoLocations = {
     world: '-180,-90,180,90' // Any geotagged tweet
 };
 
+/*
+*  Start a stream.
+*
+*  If a stream is not connected, create a new stream with the options, which contain Twitter API keys from config.js file.
+*  Then just hook up listeners to processStatus() on new data in the stream.
+*/
 TopTerms.prototype.startStream = function(options) {
   var self = this;
   var counter = 0;
@@ -94,16 +128,26 @@ TopTerms.prototype.startStream = function(options) {
     self.stream.stream(params, endpoint);
   }
 
+  /*
+  *  Display error on twitter-stream
+  */
   self.stream.on('error', function(err) {
     console.error('ERROR', err);
   });
 
+  /*
+  * When the twitter-stream captures new data form the Public Stream, it is sent to processStatus to update topTerms 
+  */
   self.stream.on('data', function(json) {
     if (!json.hasOwnProperty('text')) return; // Get rid of updates without text
     self.processStatus(json.text);
   });
 };
 
+
+/*
+*  Stop Stream by removing listeners to 'data' events
+*/
 TopTerms.prototype.stopStream = function() {
   if( !this.stream) return;
 
